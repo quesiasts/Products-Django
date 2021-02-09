e3afb878a96b8b7d753475f87391e87d754e5df9
Quesia <quesiasts@gmail.com>
2021-02-09 11:20:27 -0300

    formataçao

--- ProductModel/Product/admin.py
+++ ProductModel/Product/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+
 from Product.models import Product
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('sku', 'name', 'description', 'price', 'height',
                     'width', 'length', 'weight')
+
 
 admin.site.register(Product, ProductAdmin)
